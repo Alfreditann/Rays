054b08d9faaefdacfe1719ba3e59c5d51c2fd755
Alfreditann <alula006@osloskolen.no>
2025-11-14 09:05:13 +0100

    lightray shooting promlems

--- rays/ligth_generator.gd
+++ rays/ligth_generator.gd
@@ -9,8 +9,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		var instance = ray.instantiate()
-		instance.global_position = Vector2(global_position)
-		get_tree().current_scene.add_child(instance)
+	if$"../LigthRay".is_inside_tree():
+		$"../LigthRay".queue_free()
+	else:
+		if Input.is_action_just_pressed("ui_accept"):
+			var instance = ray.instantiate()
+			instance.global_position = Vector2(global_position)
+			get_tree().current_scene.add_child(instance)
 	
