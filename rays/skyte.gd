596f9231eaef79e0cfaef373257f4edfa0b737ad
Alfreditann <alula006@osloskolen.no>
2025-11-17 11:26:19 +0100

    timer stuff

--- rays/skyte.gd
+++ rays/skyte.gd
@@ -10,11 +10,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	look_at(get_global_mouse_position())
-	rotation -= deg_to_rad(90)
-	if Input.is_action_just_pressed("ui_accept"):
-		var instance = fireball.instantiate()
-		instance.global_position = Vector2(global_position)
-		get_tree().current_scene.add_child(instance)
+	$Timer.time_left = 0
+	if $Timer.time_left <= 0 :
+		look_at(get_global_mouse_position())
+		rotation -= deg_to_rad(90)
+		if Input.is_action_just_pressed("ui_accept"):
+			var instance = fireball.instantiate()
+			instance.global_position = Vector2(global_position)
+			get_tree().current_scene.add_child(instance)
+			$Timer.wait_time = 1
+			$Timer.start()
+	else:
+		pass
+	
 	
 	
