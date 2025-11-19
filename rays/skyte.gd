ef5ed0cc1c7828ed0a64f142c72be5e6f221e458
Alfreditann <alula006@osloskolen.no>
2025-11-19 14:36:15 +0100

    merge fix

--- rays/skyte.gd
+++ rays/skyte.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 		
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame. 
 func _process(delta):
 	#$Timer.time_left = 0
 	#if $Timer.time_left <= 0 :
