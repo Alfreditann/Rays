48b4cfe293b607a236f4daaa3c52de3e20f31cf2
Alfreditann <alula006@osloskolen.no>
2025-11-24 09:15:03 +0100

    feat:movement

--- rays/Script/character_body_2d.gd
+++ rays/Script/character_body_2d.gd
@@ -1,40 +1,61 @@
 extends CharacterBody2D
 
-var currpos = [0,0]
-@onready var anim = $AnimatedSprite2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var raycast: RayCast2D = $RayCast2D
 
+var is_moving: bool = false
+var move_dir: Vector2 = Vector2.ZERO
+var move_distance: float
+var target_position: Vector2
+
+
+func _ready():
+	# Get sprite width/height (character size)
+	move_distance = anim.sprite_frames.get_frame_texture(anim.animation, 0).get_size().x
+	target_position = position
+
+
+func update_raycast():
+	raycast.target_position = move_dir * 40
 
 
 func _physics_process(delta: float) -> void:
 
-	if Input.is_action_just_released("move_rigth"):
-		currpos[0] += 32
-		#get_node("AnimatedSprite2D").
-		look_at(self.position + Vector2(0,0))
+	# If currently moving, interpolate movement
+	if is_moving:
+		position = position.lerp(target_position, delta * 10)
+
+		# Stop when close enough
+		if position.distance_to(target_position) < 1:
+			position = target_position
+			is_moving = false
+		return
+
+
+	# --- HANDLE INPUT ONLY WHEN NOT MOVING ---
+	if Input.is_action_just_pressed("move_rigth") or Input.is_action_just_pressed("ui_right"):
+		move_dir = Vector2.RIGHT
 		anim.play("Rigth")
-	elif Input.is_action_just_released("move_left"):
-		currpos[0] -= 32
-		#get_node("AnimatedSprite2D").
-		look_at(self.position + Vector2(0,0))
+
+	elif Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("ui_left"):
+		move_dir = Vector2.LEFT
 		anim.play("Left")
-	elif Input.is_action_just_released("move_up"):
-		currpos[1] -= 32
-		#get_node("AnimatedSprite2D").
-		look_at(self.position + Vector2(0,0))
+
+	elif Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("ui_up"):
+		move_dir = Vector2.UP
 		anim.play("Back")
-	elif Input.is_action_just_released("move_down"):
-		currpos[1] += 32
-		#get_node("AnimatedSprite2D").
-		look_at(self.position + Vector2(0,0))
+
+	elif Input.is_action_just_pressed("move_down") or Input.is_action_just_pressed("ui_down"):
+		move_dir = Vector2.DOWN
 		anim.play("Front")
-	self.position = Vector2(currpos[0], currpos[1])
-	if Input.is_action_just_released("ui_right"):
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(0,-1))
-	elif Input.is_action_just_released("ui_left"):
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(0,1))
-	elif Input.is_action_just_released("ui_up"):
-		get_node("SprAnimatedSprite2D").look_at(self.position + Vector2(-1,0))
-	elif Input.is_action_just_released("ui_down"):
-		get_node("AnimatedSprite2D").look_at(self.position + Vector2(1,0))
-	
-	
+	else:
+		return  # no input, do nothing
+
+
+	# Set a new movement step
+	target_position = position + move_dir * move_distance
+	is_moving = true
+
+	# Rotate character and align raycast
+	rotation = move_dir.angle()
+	update_raycast()
