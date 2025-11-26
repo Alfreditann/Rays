857cdf91461202726f2d65a154656863095dee83
Alfreditann <alula006@osloskolen.no>
2025-11-26 11:18:56 +0100

    Update Changing_Mirror.gd

--- rays/Script/Changing_Mirror.gd
+++ rays/Script/Changing_Mirror.gd
@@ -9,11 +9,10 @@ var current_index = 0
 
 var testdDirection =[Vector2(32,0),Vector2(32,32),Vector2(0,32),Vector2(0,0)]
 var directionCounter = 0
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	set_process_input(false)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
 
@@ -28,22 +27,64 @@ func _on_activate_radius_body_exited(body: Node2D) -> void:
 func _input(event) -> void:
 	if not is_processing_input():
 		return
-	
+
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			current_index = (current_index + 1) % directions.size()
-			
-			# Move collision shape, NOT the node
-			$Speil_Hitbox.rotation_degrees = current_index * 90
+
+			if get_speil.name == "Speil_Hitbox":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox2"
+				directionCounter += 1
+
+			elif get_speil.name == "Speil_Hitbox2":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox3"
+				directionCounter += 1
+
+			elif get_speil.name == "Speil_Hitbox3":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox4"
+				directionCounter += 1
+
+			elif get_speil.name == "Speil_Hitbox4":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox"
+				directionCounter += 1
+
+
+			if directionCounter > 3:
+				directionCounter = 0
+
+
 
 		elif event.button_index == MOUSE_BUTTON_RIGHT:
 			current_index = (current_index - 1) % directions.size()
 
-	mirror.play(directions[current_index])
+			if get_speil.name == "Speil_Hitbox":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox4"
+				directionCounter -= 1
 
+			elif get_speil.name == "Speil_Hitbox2":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox"
+				directionCounter -= 1
+
+			elif get_speil.name == "Speil_Hitbox3":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox2"
+				directionCounter -= 1
+
+			elif get_speil.name == "Speil_Hitbox4":
+				get_speil.position = testdDirection[directionCounter]
+				get_speil.name = "Speil_Hitbox3"
+				directionCounter -= 1
+
+			if directionCounter < 0:
+				directionCounter = 3
+
+	mirror.play(directions[current_index])
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	emit_signal("speilHit")
-	
-
-		
