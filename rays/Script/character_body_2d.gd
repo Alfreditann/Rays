40b816b701743fa5cf4f7ee2e7041067fd1fd07b
Geniousandsmartandhumbleman <mudea002@osloskolen.no>
2025-11-27 13:32:50 +0100

    fix: bug merge

--- rays/Script/character_body_2d.gd
+++ rays/Script/character_body_2d.gd
@@ -11,7 +11,7 @@ var target_position: Vector2
 
 func _ready():
 	# Get sprite width/height (character size)
-	move_distance = anim.sprite_frames.get_frame_texture(anim.animation, 0).get_size().x
+	move_distance = anim.sprite_frames.get_frame_texture(anim.animation, anim.frame).get_size().x
 	target_position = position
 
 
@@ -23,7 +23,7 @@ func _physics_process(delta: float) -> void:
 
 	# If currently moving, interpolate movement
 	if is_moving:
-		position = position.lerp(target_position, delta * 20)
+		position = position.lerp(target_position, delta * 30)
 
 		# Stop when close enough
 		if position.distance_to(target_position) < 1:
@@ -33,19 +33,19 @@ func _physics_process(delta: float) -> void:
 
 
 	# --- HANDLE INPUT ONLY WHEN NOT MOVING ---
-	if Input.is_action_pressed("move_rigth") or Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_pressed("move_rigth") or Input.is_action_pressed("ui_right"):
 		move_dir = Vector2.RIGHT
 		anim.play("Rigth")
 
-	elif Input.is_action_pressed("move_left") or Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
 		move_dir = Vector2.LEFT
 		anim.play("Left")
 
-	elif Input.is_action_pressed("move_up") or Input.is_action_just_pressed("ui_up"):
+	elif Input.is_action_pressed("move_up") or Input.is_action_pressed("ui_up"):
 		move_dir = Vector2.UP
 		anim.play("Back")
 
-	elif Input.is_action_pressed("move_down") or Input.is_action_just_pressed("ui_down"):
+	elif Input.is_action_pressed("move_down") or Input.is_action_pressed("ui_down"):
 		move_dir = Vector2.DOWN
 		anim.play("Front")
 	else:
