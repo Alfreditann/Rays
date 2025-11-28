dadb0d16d0edbb5b9dd9375285615a2f0c5cebe5
Geniousandsmartandhumbleman <mudea002@osloskolen.no>
2025-11-28 09:50:06 +0100

    fix:merge bug

--- rays/Script/character_body_2d.gd
+++ rays/Script/character_body_2d.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-@onready var raycast: RayCast2D = $RayCast2D
+#@onready var raycast: RayCast2D = $RayCast2D
 
 var is_moving: bool = false
 var move_dir: Vector2 = Vector2.ZERO
 var move_distance: float
 var target_position: Vector2
 
-
 func _ready():
 	# Get sprite width/height (character size)
 	move_distance = anim.sprite_frames.get_frame_texture(anim.animation, anim.frame).get_size().x
 	target_position = position
 
-
-func update_raycast():
-	raycast.target_position = move_dir * 40
-
+#func update_raycast():
+	##raycast.cast_to = move_dir.normalized() * 40.0
 
 func _physics_process(delta: float) -> void:
 
@@ -25,17 +22,15 @@ func _physics_process(delta: float) -> void:
 	if is_moving:
 		position = position.lerp(target_position, delta * 30)
 
-		# Stop when close enough
 		if position.distance_to(target_position) < 1:
 			position = target_position
 			is_moving = false
 		return
 
-
-	# --- HANDLE INPUT ONLY WHEN NOT MOVING ---
-	if Input.is_action_pressed("move_rigth") or Input.is_action_pressed("ui_right"):
+	# --- INPUT ---
+	if Input.is_action_pressed("move_right") or Input.is_action_pressed("ui_right"):
 		move_dir = Vector2.RIGHT
-		anim.play("Rigth")
+		anim.play("Right")
 
 	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("ui_left"):
 		move_dir = Vector2.LEFT
@@ -49,12 +44,11 @@ func _physics_process(delta: float) -> void:
 		move_dir = Vector2.DOWN
 		anim.play("Front")
 	else:
-		return  # no input, do nothing
+		return
 
-
-	# Set a new movement step
+	# Move one tile
 	target_position = position + move_dir * move_distance
 	is_moving = true
 
-	# Rotate character and align raycast
-	update_raycast()
+	# Update raycast
+	#update_raycast()
